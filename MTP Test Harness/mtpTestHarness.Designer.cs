f6a4aa9dd256c2a05b0fd49a0cbd6964406c3f05
edma8378 <edma8378@colorado.edu>
2014-07-28 16:11:35 -0600

    Initial
    This test the mtp connectivity of an Agilent Easy File Access device.

---
+++ MTP Test Harness/mtpTestHarness.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace MTP_Test_Harness
+{
+    partial class mtpTestHarness
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.test1B = new System.Windows.Forms.Button();
+            this.test2B = new System.Windows.Forms.Button();
+            this.test4B = new System.Windows.Forms.Button();
+            this.test3B = new System.Windows.Forms.Button();
+            this.addressTB = new System.Windows.Forms.TextBox();
+            this.connectB = new System.Windows.Forms.Button();
+            this.deviceLB = new System.Windows.Forms.Label();
+            this.launchB = new System.Windows.Forms.Button();
+            this.disconnectB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // test1B
+            // 
+            this.test1B.Location = new System.Drawing.Point(10, 64);
+            this.test1B.Name = "test1B";
+            this.test1B.Size = new System.Drawing.Size(78, 23);
+            this.test1B.TabIndex = 0;
+            this.test1B.Text = "Test 1";
+            this.test1B.UseVisualStyleBackColor = true;
+            this.test1B.Click += new System.EventHandler(this.test1B_Click);
+            // 
+            // test2B
+            // 
+            this.test2B.Location = new System.Drawing.Point(103, 64);
+            this.test2B.Name = "test2B";
+            this.test2B.Size = new System.Drawing.Size(78, 23);
+            this.test2B.TabIndex = 1;
+            this.test2B.Text = "Test 2";
+            this.test2B.UseVisualStyleBackColor = true;
+            this.test2B.Click += new System.EventHandler(this.test2B_Click);
+            // 
+            // test4B
+            // 
+            this.test4B.Location = new System.Drawing.Point(103, 93);
+            this.test4B.Name = "test4B";
+            this.test4B.Size = new System.Drawing.Size(78, 23);
+            this.test4B.TabIndex = 3;
+            this.test4B.Text = "Test 4";
+            this.test4B.UseVisualStyleBackColor = true;
+            this.test4B.Click += new System.EventHandler(this.test4B_Click);
+            // 
+            // test3B
+            // 
+            this.test3B.Location = new System.Drawing.Point(10, 93);
+            this.test3B.Name = "test3B";
+            this.test3B.Size = new System.Drawing.Size(78, 23);
+            this.test3B.TabIndex = 2;
+            this.test3B.Text = "Test 3";
+            this.test3B.UseVisualStyleBackColor = true;
+            this.test3B.Click += new System.EventHandler(this.test3B_Click);
+            // 
+            // addressTB
+            // 
+            this.addressTB.Location = new System.Drawing.Point(10, 10);
+            this.addressTB.Name = "addressTB";
+            this.addressTB.Size = new System.Drawing.Size(171, 22);
+            this.addressTB.TabIndex = 4;
+            this.addressTB.Text = "address";
+            // 
+            // connectB
+            // 
+            this.connectB.Location = new System.Drawing.Point(187, 9);
+            this.connectB.Name = "connectB";
+            this.connectB.Size = new System.Drawing.Size(88, 23);
+            this.connectB.TabIndex = 5;
+            this.connectB.Text = "Connect";
+            this.connectB.UseVisualStyleBackColor = true;
+            this.connectB.Click += new System.EventHandler(this.connectB_Click);
+            // 
+            // deviceLB
+            // 
+            this.deviceLB.BackColor = System.Drawing.Color.Red;
+            this.deviceLB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.deviceLB.Location = new System.Drawing.Point(10, 38);
+            this.deviceLB.Name = "deviceLB";
+            this.deviceLB.Size = new System.Drawing.Size(171, 20);
+            this.deviceLB.TabIndex = 6;
+            this.deviceLB.Text = "Device Not Connected";
+            // 
+            // launchB
+            // 
+            this.launchB.Location = new System.Drawing.Point(187, 64);
+            this.launchB.Name = "launchB";
+            this.launchB.Size = new System.Drawing.Size(88, 52);
+            this.launchB.TabIndex = 7;
+            this.launchB.Text = "Launch ACE";
+            this.launchB.UseVisualStyleBackColor = true;
+            this.launchB.Click += new System.EventHandler(this.launchB_Click);
+            // 
+            // disconnectB
+            // 
+            this.disconnectB.Enabled = false;
+            this.disconnectB.Location = new System.Drawing.Point(187, 35);
+            this.disconnectB.Name = "disconnectB";
+            this.disconnectB.Size = new System.Drawing.Size(88, 23);
+            this.disconnectB.TabIndex = 8;
+            this.disconnectB.Text = "Disconnect";
+            this.disconnectB.UseVisualStyleBackColor = true;
+            this.disconnectB.Click += new System.EventHandler(this.disconnectB_Click);
+            // 
+            // mtpTestHarness
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 124);
+            this.Controls.Add(this.disconnectB);
+            this.Controls.Add(this.launchB);
+            this.Controls.Add(this.deviceLB);
+            this.Controls.Add(this.connectB);
+            this.Controls.Add(this.addressTB);
+            this.Controls.Add(this.test4B);
+            this.Controls.Add(this.test3B);
+            this.Controls.Add(this.test2B);
+            this.Controls.Add(this.test1B);
+            this.Name = "mtpTestHarness";
+            this.Text = "MTP Test Harness";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button test1B;
+        private System.Windows.Forms.Button test2B;
+        private System.Windows.Forms.Button test4B;
+        private System.Windows.Forms.Button test3B;
+        private System.Windows.Forms.TextBox addressTB;
+        private System.Windows.Forms.Button connectB;
+        private System.Windows.Forms.Label deviceLB;
+        private System.Windows.Forms.Button launchB;
+        private System.Windows.Forms.Button disconnectB;
+    }
+}
+
---
+++ MTP Test Harness/mtpTestHarness.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MTP_Test_Harness
+{
+    public partial class mtpTestHarness : Form
+    {
+        bool connected;
+        const String localdir = "";//location of what we are sending to the inst\
+        String address = "";
+        public mtpTestHarness()
+        {
+            InitializeComponent();
+            connected = false;
+        }
+
+        private void connectB_Click(object sender, EventArgs e)
+        {
+            String input = addressTB.Text;
+            //call conection here
+            int resourceManager = 0, viError;
+            int session = 0;
+            viError = visa32.viOpenDefaultRM(out resourceManager);
+            viError = visa32.viOpen(resourceManager, input,
+                visa32.VI_NO_LOCK, visa32.VI_TMO_IMMEDIATE, out session);
+            viError = visa32.viPrintf(session, "*IDN?\n");
+            System.Text.StringBuilder idnString =
+                new System.Text.StringBuilder(1000);
+            viError = visa32.viScanf(session, "%1000t", idnString);
+            System.Windows.Forms.MessageBox.Show(idnString.ToString());
+            
+            if (viError < visa32.VI_SUCCESS)
+            {
+                deviceLB.BackColor = Color.Red;
+                deviceLB.Text = "Device Not Connected";
+                connected = false;
+                address = "";
+            }
+            else
+            {
+                deviceLB.BackColor = Color.Green;
+                deviceLB.Text = "Device Connected";
+                address = input;
+                connected = true;
+                addressTB.Enabled = false;
+                //set default directory
+                String cmd = "MMEM:CDIR '/User Files'";
+                viError = visa32.viPrintf(session, cmd);
+                //error check here               
+                if (viError < visa32.VI_SUCCESS) { goto EndConnect; }
+                viError = visa32.viPrintf(session, "SYST:ERR?\n");                
+                System.Text.StringBuilder retString =
+                    new System.Text.StringBuilder(1000);
+                viError = visa32.viScanf(session, "%1000t", retString);
+                System.Windows.Forms.MessageBox.Show(retString.ToString());
+                
+            }
+
+            connectB.Enabled = !connected;
+            disconnectB.Enabled = connected;
+
+            visa32.viClose(session);
+            visa32.viClose(resourceManager);
+            return;
+
+        EndConnect:
+            System.Windows.Forms.MessageBox.Show("Unable to connect to device.");
+            return;
+        }
+
+        private void disconnectB_Click(object sender, EventArgs e)
+        {
+            addressTB.Enabled = true;
+            address = "";
+            connectB.Enabled = true;
+            disconnectB.Enabled = false;
+        }
+
+        private void test1B_Click(object sender, EventArgs e)
+        {
+            //send a file
+            int ret = create_file("test");
+            if (ret != 0)
+            {
+                //unable to send
+            }
+            else
+            {
+                //System.Windows.Forms.MessageBox.Show("WE DID IT!");
+            }
+        }
+
+        private void test2B_Click(object sender, EventArgs e)
+        {
+            //create a empty folder
+            //send a file
+            int ret = create_folder("test");
+            if (ret != 0)
+            {
+                //unable to send
+            }
+            else
+            {
+                //System.Windows.Forms.MessageBox.Show("WE DID IT!");
+            }
+        }
+
+        
+
+        private void test3B_Click(object sender, EventArgs e)
+        {
+            //send a folder full of files
+
+        }
+
+        private void test4B_Click(object sender, EventArgs e)
+        {
+            //send a folder with nested folders
+
+        }
+
+        private void launchB_Click(object sender, EventArgs e)
+        {
+            Process.Start("C:\\Program Files\\Agilent\\IO Libraries Suite\\AgilentConnectionExpert.exe");
+        }
+
+        private int create_file(String filename)
+        {
+            int resourceManager = 0, viError;
+            int session = 0;
+            viError = visa32.viOpenDefaultRM(out resourceManager);
+            viError = visa32.viOpen(resourceManager, address,
+                visa32.VI_NO_LOCK, visa32.VI_TMO_IMMEDIATE, out session);
+            int nDigits = 20 , nBytes = 66;
+            String cmd = "MMEM:DATA \""+filename+"\" ,#"+nDigits+nBytes;
+            viError = visa32.viPrintf(session, cmd+"\n");
+            
+            if (viError < visa32.VI_SUCCESS)
+            {
+                System.Text.StringBuilder error =
+                    new System.Text.StringBuilder(256);
+                visa32.viStatusDesc(session, viError, error);
+                System.Windows.Forms.MessageBox.Show(error.ToString());
+                visa32.viClose(session);
+                visa32.viClose(resourceManager);
+                return 1;
+            }
+            viError = visa32.viPrintf(session, "SYST:ERR?\n");
+            System.Text.StringBuilder idnString = new System.Text.StringBuilder(1000);
+            viError = visa32.viScanf(session, "%1000s", idnString);
+            System.Windows.Forms.MessageBox.Show(idnString.ToString());
+            //check returned string
+            char[] delimiters = {','};
+            string[] text = idnString.ToString().Split(delimiters);
+            int decVal = -1;
+            try
+            {
+                decVal = Convert.ToInt32(text[0]);
+            }
+            catch (FormatException e)
+            {
+
+            }
+            
+            if (decVal < 0)
+            {
+                visa32.viClose(session);
+                visa32.viClose(resourceManager);
+                return 1;
+            }
+            else
+            {
+                visa32.viClose(session);
+                visa32.viClose(resourceManager);
+                return 0;
+            }
+                    
+        }
+
+        private int create_folder(string folderName)
+        {
+            int resourceManager = 0, viError;
+            int session = 0;
+            viError = visa32.viOpenDefaultRM(out resourceManager);
+            viError = visa32.viOpen(resourceManager, address,
+                visa32.VI_NO_LOCK, visa32.VI_TMO_IMMEDIATE, out session);
+            
+            String cmd = "MMEM:CDIR \"" + folderName + "\" ";
+            viError = visa32.viPrintf(session, cmd + "\n");
+
+            if (viError < visa32.VI_SUCCESS) //command sent correctly
+            {
+                System.Text.StringBuilder error =
+                    new System.Text.StringBuilder(256);
+                visa32.viStatusDesc(session, viError, error);
+                System.Windows.Forms.MessageBox.Show(error.ToString());
+                visa32.viClose(session);
+                visa32.viClose(resourceManager);
+                return 1;
+            }
+            viError = visa32.viPrintf(session, "SYST:ERR?\n");
+            System.Text.StringBuilder idnString = new System.Text.StringBuilder(1000);
+            viError = visa32.viScanf(session, "%1000s", idnString);
+            System.Windows.Forms.MessageBox.Show(idnString.ToString());
+            //check returned string
+            char[] delimiters = { ',' };
+            string[] text = idnString.ToString().Split(delimiters);
+            int decVal = -1;
+            try
+            {
+                decVal = Convert.ToInt32(text[0]);
+            }
+            catch (FormatException e)
+            {
+
+            }
+
+            if (decVal < 0)
+            {
+                visa32.viClose(session);
+                visa32.viClose(resourceManager);
+                return 1;
+            }
+            else
+            {
+                visa32.viClose(session);
+                visa32.viClose(resourceManager);
+                return 0;
+            }
+            
+        }
+    }
+}
